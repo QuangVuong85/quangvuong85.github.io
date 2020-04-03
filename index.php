e27bc72123db2fb64976191ae3d70a0cd5678550
QuangVuong85 <quangvuong0805@gmail.com>
2020-04-04 01:56:10 +0700

    add website

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Đinh Quang Vương - #ĐQV</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="icon" href="favicon.ico" type="image/x-icon">
+		<link rel="stylesheet" href="assets/css/main.css" />
+		
+		<meta name="description" content="Đinh Quang Vương - #ĐQV" />
+		<meta itemprop="image" content="https://lythanhphuc.com/images/ltp-cover.jpg" />
+		<meta property="og:title" content="Đinh Quang Vương" />
+		<meta property="og:url" content="http://vuongdq.com" />
+		<meta property="og:image" content="https://lythanhphuc.com/images/ltp-cover.jpg" />
+		<meta property="fb:admins" content="100002894018446" />
+		<meta property="fb:app_id" content="141841956146846" />
+		<meta property="og:type" content="website" />
+		<meta property="og:description" content="Đinh Quang Vương - #ĐQV" />
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<span class="avatar"><img src="images/3537814.jpg" alt="" /></span>
+						<h1>Xin chào, tôi là <strong>Đinh Quang Vương</strong>.</h1>
+						<ul class="icons">
+							<li><a href="https://www.facebook.com/vuongdq85" title="Facebook"><i class="fab fa-facebook" style="font-size:48px"></i></a></li>
+							<li><a href="https://m.me/vuongdq85" title="Messenger"><i class="fab fa-facebook-messenger" style="font-size:48px"></i></a></li>
+							<li><a href="https://discordapp.com/" title="Discord"><i class="fab fa-discord" style="font-size:48px"></i></a></li>
+							<!--<li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon style2 fa-500px"><span class="label">500px</span></a></li>
+							<li><a href="#" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>-->
+						</ul>
+					</header>
+					
+				<!-- Main -->
+					<!--<section id="main">-->
+
+						<!-- Thumbnails -->
+							<!--<section class="thumbnails">
+								<div>
+									<a href="images/fulls/01.jpg">
+										<img src="images/thumbs/01.jpg" alt="" />
+										<h3>Lorem ipsum dolor sit amet</h3>
+									</a>
+									<a href="images/fulls/02.jpg">
+										<img src="images/thumbs/02.jpg" alt="" />
+										<h3>Lorem ipsum dolor sit amet</h3>
+									</a>
+								</div>
+								<div>
+									<a href="images/fulls/03.jpg">
+										<img src="images/thumbs/03.jpg" alt="" />
+										<h3>Lorem ipsum dolor sit amet</h3>
+									</a>
+									<a href="images/fulls/04.jpg">
+										<img src="images/thumbs/04.jpg" alt="" />
+										<h3>Lorem ipsum dolor sit amet</h3>
+									</a>
+									<a href="images/fulls/05.jpg">
+										<img src="images/thumbs/05.jpg" alt="" />
+										<h3>Lorem ipsum dolor sit amet</h3>
+									</a>
+								</div>
+								<div>
+									<a href="images/fulls/06.jpg">
+										<img src="images/thumbs/06.jpg" alt="" />
+										<h3>Lorem ipsum dolor sit amet</h3>
+									</a>
+									<a href="images/fulls/07.jpg">
+										<img src="images/thumbs/07.jpg" alt="" />
+										<h3>Lorem ipsum dolor sit amet</h3>
+									</a>
+								</div>
+							</section>-->
+
+					<!--</section>-->
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p>&copy; 2020 - Đinh Quang Vương. Design: <a href="http://templated.co">TEMPLATED</a>.</p>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery-3.4.1.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/main.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+	</body>
+</html>
